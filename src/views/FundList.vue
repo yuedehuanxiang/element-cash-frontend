eaa262eeb2c38b227baf20d61d80c3bb7329433a
chenshuai <384619043@qq.com>
2018-11-28 07:13:09 +0800

    完成分页功能

--- src/views/FundList.vue
+++ src/views/FundList.vue
@@ -11,63 +11,63 @@
     </div>
     <div class="table_container">
       <el-table
-      max-height="450"
-      border
-      v-if="tableData.length > 0 "
-      :data="tableData"
-      style="width: 100%">
-      <el-table-column
-        align="center"
-        label="序号"
-        type="index"
-        width="70">
-      </el-table-column>
-      <el-table-column
-        align="center"
-        prop="date"
-        label="创建时间"
-        width="250">
-        <template slot-scope="scope">
-          <i class="el-icon-time"></i>
-          <span style="margin-left: 10px">{{ scope.row.date }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column prop="type" label="收支类型" align='center' width="150">
-          </el-table-column>
-          <el-table-column prop="describe" label="收支描述" align='center' width="180">
-          </el-table-column>
-          <el-table-column prop="income" label="收入" align='center' width="150">
-            <template slot-scope="scope">
-              <span style="color: #00d053">{{ scope.row.income }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column prop="expend" label="支出" align='center' width="180">
-            <template slot-scope="scope">
-              <span style="color: #f56767">{{ scope.row.expend }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column prop="cash" label="账户现金" align='center' width="150">
-            <template slot-scope="scope">
-              <span style="color: #3db3ff">{{ scope.row.cash }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column prop="remark" label="备注" align='center' width="180">
-          </el-table-column>
-          <el-table-column prop="operation"
-            label="操作" align='center' 
-            fixed="right" width="320">
-            <template slot-scope="scope">
-              <el-button type="warning" icon="edit" size="small"
-              @click="handleEdit(scope.$index, scope.row)">
-                编辑
-              </el-button>
-              <el-button icon="delete" size="small" type="danger"
-              @click="handleDelete(scope.$index, scope.row)">
-                删除
-              </el-button>
-            </template>
-          </el-table-column>
+        max-height="450"
+        border
+        v-if="tableData.length > 0 "
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column type="index" label="序号" align="center" width="70">
+        </el-table-column>
+        <el-table-column label="创建时间" align='center' width="250" prop="date">
+          <template slot-scope="scope">
+            <i class="el-icon-time"></i>
+            <span style="margin-left: 10px">{{ scope.row.date }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="type" label="收支类型" align='center' width="150">
+        </el-table-column>
+        <el-table-column prop="describe" label="收支描述" align='center' width="180">
+        </el-table-column>
+        <el-table-column prop="income" label="收入" align='center' width="150">
+          <template slot-scope="scope">
+            <span style="color: #00d053">{{ scope.row.income }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="expend" label="支出" align='center' width="180">
+          <template slot-scope="scope">
+            <span style="color: #f56767">{{ scope.row.expend }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="cash" label="账户现金" align='center' width="150">
+          <template slot-scope="scope">
+            <span style="color: #3db3ff">{{ scope.row.cash }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="remark" label="备注" align='center' width="180">
+        </el-table-column>
+        <el-table-column prop="operation" label="操作" align='center' fixed="right" width="320">
+          <template slot-scope="scope">
+            <el-button type="warning" icon="edit" size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button icon="delete" size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
+      <!-- 分页  -->
+      <el-row>
+        <el-col :span="24">
+          <div class="pagination">
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page.sync="paginations.page_index"
+              :page-sizes="paginations.page_sizes"
+              :page-size="paginations.page_size"
+              :layout="paginations.layout"
+              :total="paginations.total">
+            </el-pagination>
+          </div>
+        </el-col>
+      </el-row>
     </div>
     <fund-dialog :formData="formData" @update="getProfile" :dialog="dialog"></fund-dialog>
   </div>
@@ -79,7 +79,15 @@ export default {
   name: "fundList",
   data() {
     return {
+      paginations: {
+        page_index: 1, //当前位于哪页
+        total: 0, //0
+        page_size: 5, //一页显示多少条
+        page_sizes: [5, 10, 15, 20], //每页显示多少条
+        layout: "total,sizes,prev,pager,next,jumper" //翻页属性
+      },
       tableData: [],
+      allTableData: [],
       formData: {
         type: "",
         describe: "",
@@ -108,10 +116,43 @@ export default {
     this.getProfile();
   },
   methods: {
+    handleSizeChange(page_size) {
+      // 切换size
+      this.paginations.page_index = 1;
+      this.paginations.page_size = page_size;
+      this.tableData = this.allTableData.filter((item, index) => {
+        return index < this.paginations.page_size;
+      });
+    },
+    handleCurrentChange(page) {
+      // 获取当前页开始下标
+      let index = this.paginations.page_size * (page - 1);
+      // 数据的总数
+      let nums = this.paginations.page_size * page;
+      // 容器
+      let tables = [];
+      for (let i = index; i < nums; i++) {
+        if (this.allTableData[i]) {
+          tables.push(this.allTableData[i]);
+        }
+      }
+      this.tableData = tables;
+    },
     getProfile() {
       this.$axios.get("/api/profiles").then(res => {
         console.log(res);
-        this.tableData = res.data;
+        this.allTableData = res.data;
+        this.setPaginations();
+      });
+    },
+    setPaginations() {
+      // 分页属性设置
+      this.paginations.total = this.allTableData.length;
+      this.paginations.page_index = 1;
+      this.paginations.page_size = 5;
+      // 设置默认的分页数据
+      this.tableData = this.allTableData.filter((item, index) => {
+        return index < this.paginations.page_size;
       });
     },
     handleEdit(index, row) {
