6553c713c34ec8cea479977acf62268e4b4a31b8
chenshuai <384619043@qq.com>
2018-12-01 09:17:14 +0800

    mac

--- src/http.js
+++ src/http.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { Message, Loading } from "element-ui";
 import router from "./router";
 
+//mac
+
 let loading;
 function startLoading() {
   loading = Loading.service({
